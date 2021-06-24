351346e5ef8d8d533f75c9dedbda9049c22e45ba
Mikaela Duguil <kimmieduggz@gmail.com>
2021-06-24 12:20:53 -0700

    created loading when no selected video

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import SearchBar from './SearchBar'
 import youtube from '../apis/youtube'
 import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 
 export default class App extends React.Component {
   state = {
@@ -19,13 +20,14 @@ export default class App extends React.Component {
   }
 
   onVideoSelect = video => {
-    console.log('app: ', video)
+    this.setState({selectedVideo: video})
   }
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit}/>
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           videos={this.state.videos}
           onVideoSelect={this.onVideoSelect}
