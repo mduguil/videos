3eb254b640ce7d09e6c41f7695ccdba6568094fc
Mikaela Duguil <kimmieduggz@gmail.com>
2021-06-24 12:51:33 -0700

    search renders first video

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,7 +16,10 @@ export default class App extends React.Component {
         q: term
       }
     })
-    this.setState({ videos: response.data.items})
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    })
   }
 
   onVideoSelect = video => {
@@ -27,11 +30,19 @@ export default class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit}/>
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={this.onVideoSelect}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
